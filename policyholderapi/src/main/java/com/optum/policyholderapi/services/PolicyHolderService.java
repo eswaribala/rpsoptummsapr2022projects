2f68d8c1fce0815cfa5c9e808970358d30e93712
eswaribala <parameswaribala@gmail.com>
2022-04-25 13:33:34 +0530

    updated

---
+++ policyholderapi/src/main/java/com/optum/policyholderapi/services/PolicyHolderService.java
@@ -0,0 +1,49 @@
+package com.optum.policyholderapi.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.optum.policyholderapi.models.PolicyHolder;
+import com.optum.policyholderapi.repositories.PolicyHolderRepo;
+
+@Service
+public class PolicyHolderService {
+	@Autowired
+	private PolicyHolderRepo policyHolderRepo;
+	
+	//insert
+	
+	public PolicyHolder addPolicyHolder(PolicyHolder policyHolder) {
+		return this.policyHolderRepo.save(policyHolder);
+	}
+	
+	//select all
+	public List<PolicyHolder> getAllPolicyHolders(){
+		return this.policyHolderRepo.findAll();
+	}
+	
+	//select
+	
+	public PolicyHolder getPolicyHolderById(long policyNo) {
+		return this.policyHolderRepo.findById(policyNo).orElse(null);
+	}
+	
+	//delete
+	
+	public boolean deletePolicyHolderById(long policyNo) {
+		this.policyHolderRepo.deleteById(policyNo);
+		if(this.getPolicyHolderById(policyNo)==null)
+			return true;
+		else
+			return false;
+		
+	}
+	
+	//update
+	
+	
+	
+
+}
